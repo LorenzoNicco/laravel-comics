f22972e12e0dc3747a8dc9c67a52d3270df16930
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-09 16:31:47 +0100

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="logo-container col-2">
+                <img src="{{ Vite::asset('resources/img/laravel-comics/images/dc-logo.png') }}" alt="">
+            </div>
+    
+            <nav class="col-10">
+                <ul class="list-unstyled d-flex justify-content-between">
+                    <li>
+                        <a>CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a>COMICS</a>
+                    </li>
+                    <li>
+                        <a>MOVIES</a>
+                    </li>
+                    <li>
+                        <a>TV</a>
+                    </li>
+                    <li>
+                        <a>GAMES</a>
+                    </li>
+                    <li>
+                        <a>COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a>VIDEOS</a>
+                    </li>
+                    <li>
+                        <a>FANS</a>
+                    </li>
+                    <li>
+                        <a>NEWS</a>
+                    </li>
+                    <li>
+                        <a>SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
+</header>
